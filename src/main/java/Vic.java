c26387b2c41110502967191bd6672968e2de257e
Victoria281 <amandaquek281@gmail.com>
2025-01-30 12:37:40 +0800

    rename chatbot

---
+++ src/main/java/Vic.java
@@ -0,0 +1,14 @@
+public class Vic {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String name = "Vic";
+        String intro = " Hello! I'm "
+                + name
+                +"\n"
+                + " What can I do for you?\n"
+                + line
+                + " Bye. Hope to see you again soon!\n"
+                + line;
+        System.out.println(intro);
+    }
+}
