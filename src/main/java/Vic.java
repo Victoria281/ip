dde43fd1d4ceaa7a54224595a92168e4f6e62002
Victoria281 <amandaquek281@gmail.com>
2025-01-30 15:11:40 +0800

    Add add, list feature

--- src/main/java/Vic.java
+++ src/main/java/Vic.java
@@ -12,6 +12,7 @@ public class Vic {
             + "\t What can I do for you?\n"
             + line;
     private static final String outro = line + "\t Bye. Hope to see you again soon!\n" + line;
+    private static final ArrayList<String> toDoList = new ArrayList<String>();
 
     /**
      * Returns reply message
@@ -23,6 +24,27 @@ public class Vic {
         return line + "\t " + msg + "\n" + line;
     }
 
+    /**
+     * add item to toDoList
+     *
+     * @param item item to add
+     */
+    static void addToDoItem(String item) {
+        toDoList.add(item);
+        System.out.println(replyFormatter("added: " + item));
+    }
+
+    /**
+     * prints the toDoList
+     */
+    static void printToDoList() {
+        System.out.println(line);
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println("\t " + (i + 1) + ". " + toDoList.get(i));
+        }
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -31,7 +53,11 @@ public class Vic {
         String response = input.nextLine();
 
         while (!response.equals("bye")) {
-            System.out.println(replyFormatter(response));
+            if (response.equals("list")) {
+                printToDoList();
+            } else {
+                addToDoItem(response);
+            }
             response = input.nextLine();
         }
         System.out.println(outro);
